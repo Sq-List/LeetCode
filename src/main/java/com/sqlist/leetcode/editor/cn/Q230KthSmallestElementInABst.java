645896a4191f47333070714461d4063d7435d1fe
Sq-List <574146616@qq.com>
2021-10-18 11:45:19 +0800

    Q230 中序遍历-递归.

--- src/main/java/com/sqlist/leetcode/editor/cn/Q230KthSmallestElementInABst.java
+++ src/main/java/com/sqlist/leetcode/editor/cn/Q230KthSmallestElementInABst.java
@@ -40,6 +40,8 @@ package com.sqlist.leetcode.editor.cn;
 
 import com.sqlist.leetcode.editor.cn.pojo.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -48,15 +50,95 @@ import java.util.Stack;
  * @createTime 2021-10-17 21:13:51
  **/
 public class Q230KthSmallestElementInABst {
+
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Q230KthSmallestElementInABst().new Solution();
+        // System.out.println(solution.kthSmallest(stringToTreeNode("[3,1,4,null,2]"), 1));
+        System.out.println(solution.kthSmallest(stringToTreeNode("[5,3,6,2,4,null,null,1]"), 3));
+        System.out.println("-------------------");
+        System.out.println(solution.kthSmallest(stringToTreeNode("[5,3,6,2,4,null,null,1]"), 4));
     }
     
     //leetcode submit region begin(Prohibit modification and deletion)
     /**
-     * 中序遍历
+     * 中序遍历 - 递归
      */
     class Solution {
+        private int res;
+        private int rank;
+
+        public int kthSmallest(TreeNode root, int k) {
+            rank = 0;
+            middle(root, k);
+            return res;
+        }
+
+        private void middle(TreeNode root, int k) {
+            if (root == null || rank > k) {
+                return;
+            }
+
+            middle(root.left, k);
+            System.out.println(root.val);
+            rank ++;
+            if (k == rank) {
+                res = root.val;
+                return;
+            }
+            middle(root.right, k);
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    /**
+     * 中序遍历 - 迭代
+     * 迭代可以在找到答案后直接停止 不需要遍历整棵树
+     */
+    class SolutionMiddleIter {
         public int kthSmallest(TreeNode root, int k) {
             Stack<TreeNode> stack = new Stack<>();
             while (root != null || !stack.empty()) {
@@ -75,6 +157,4 @@ public class Q230KthSmallestElementInABst {
             return root.val;
         }
     }
-    //leetcode submit region end(Prohibit modification and deletion)
-
 }
